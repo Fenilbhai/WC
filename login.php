a84d4d1339d5afc12d1812eddf36cd15d0ca0b1e
FENIL VIPULBHAI DHOLARIYA <fenildholariya35@gmail.com>
2022-04-26 14:00:46 +0530

    WC

---
+++ login.php
@@ -0,0 +1,26 @@
+<?
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$con = new mysql("localhost","root","","login");
+if($con->connect_error){
+	die("Failed to connect : ".$con->connect_error)
+}
+else{
+	$stmt = $con->prepare("select * from login page where email = ?");
+	$stmt->bind_param("s", $email);
+	$stmt->execute();
+	$stmt_result = $stmt->get_result();
+	if($stmt_result->num_rows > 0){
+         $data = $stmt_result->fetch_assoc();
+         if($data['password'] === $password){
+         	echo "<h2>Login Successfully</h2>";
+         }
+         else{
+         	echo "<h2>Invalid Email or password</h2>";
+         }
+	}else{
+		echo "<h2>Invalid Email or password</h2>";
+	}
+}
+?>
